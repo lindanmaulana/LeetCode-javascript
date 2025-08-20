da6ea4c8253f371cf697d70511e648ff69eed603
Lindan Maulana <lindanstemga@gmail.com>
2025-08-20 13:10:56 +0700

    added 2 soal

--- javascript/soal-terjawab.js
+++ javascript/soal-terjawab.js
@@ -184,3 +184,117 @@ const result3 = reduce(nums3, fn3, init3)
 console.log({result1})
 console.log({result2})
 console.log({result3})
+
+// soal 7 Function Composition
+var compose = function(functions) {
+    return function(x) {
+        let result = x
+
+        for(let i = functions.length - 1; i >= 0; i--) {
+          result = functions[i](result)
+        }
+
+        return result
+    }
+};
+
+const functions1 = [x => x + 1, x => x * x, x => 2 * x]
+const function1 = 4
+const run1 = compose(functions1)
+const runGo1 = run1(function1)
+console.log(runGo1)
+
+
+const functions2 = [x => 10 * x, x => 10 * x, x => 10 * x]
+const function2 = 1
+const run2 = compose(functions2)
+const runGo2 = run2(function2)
+console.log(runGo2)
+
+
+// soal 8 Return Length of Arguments Passed
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+// soal 9 Allow One Function Call
+const once = function (fn) {
+  let cals = false;
+
+  return function (...args) {
+    if(cals) return undefined
+    cals = true
+
+    return fn(...args)
+  };
+};
+
+// const fn1 = (a, b, c) => a + b + c;
+// const arr1 = [
+//   [1, 2, 3],
+//   [2, 3, 6],
+// ];
+
+// const fn2 = (a, b, c) => a * b * c;
+// const arr2 = [
+//   [5, 7, 4],
+//   [2, 3, 6],
+//   [4, 6, 8],
+// ];
+
+const setFn1 = once(fn1);
+console.log(setFn1(arr1));
+
+
+// soal 10
+function memoize(fn) {
+  const cache = new Map();  // untuk menyimpan hasil pemanggilan sebelumnya
+  let callCount = 0;        // menghitung berapa kali fn benar-benar dipanggil
+
+  const memoizedFn = (...args) => {
+    console.log({args})
+    const key = JSON.stringify(args); // jadikan array argumen string agar unik
+
+    if (cache.has(key)) {
+        console.log({has: cache.has(key)})
+        console.log({hey: cache.get(key)})
+      return cache.get(key); // kembalikan nilai cache
+    }
+
+    console.log({cache})
+    
+    const result = fn(...args);
+    cache.set(key, result);
+    callCount++;
+
+    console.log({cache})
+    console.log({key})
+    return result;
+  };
+
+  // method tambahan untuk mendapatkan call count
+  memoizedFn.getCallCount = () => callCount;
+
+  return memoizedFn;
+}
+
+const fn = (a, b) => a + b
+const fnRun = memoize(fn)
+console.log(fnRun(2, 1))
+console.log(fnRun(2, 1))
+
+
+// soal 11
+var addTwoPromises = async function(promise1, promise2) {
+    let val1, val2
+
+    await promise1.then(val => val1 = val)
+    await promise2.then(val => val2 = val)
+    const result = val1 + val2
+
+    return result
+};
+
+const promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20))
+const promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+
--- javascript/soal.js
+++ javascript/soal.js
@@ -1,24 +1,10 @@
-var compose = function(functions) {
-    return function(x) {
-        let result = x
+async function sleep(millis) {
+    let res
 
-        for(let i = functions.length - 1; i >= 0; i--) {
-          result = functions[i](result)
-        }
+    setTimeout(() => val = millis, millis)
 
-        return result
-    }
-};
+    return res
+}
 
-const functions1 = [x => x + 1, x => x * x, x => 2 * x]
-const function1 = 4
-const run1 = compose(functions1)
-const runGo1 = run1(function1)
-console.log(runGo1)
-
-
-const functions2 = [x => 10 * x, x => 10 * x, x => 10 * x]
-const function2 = 1
-const run2 = compose(functions2)
-const runGo2 = run2(function2)
-console.log(runGo2)
+const date = Date.now()
+console.log(sleep(100).then(() => Date.now() - date))
